beb31ffcb540705990fa9391c47a63295f449846
Everton José <everton7ej@gmail.com>
2022-03-09 18:23:20 -0300

    update

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <form action="upload.php" method="post" enctype="multipart/form-data">
   <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="text" name="nome" id="nome" placeholder="nome: ">
+  <input type="text" name="empresas" id="empresas" placeholder="empresas: ">
   <input type="submit" value="Upload Image" name="submit">
 </form>
 
